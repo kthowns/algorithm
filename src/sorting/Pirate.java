11d892883468c33d2b5640e489c60062044a4c58
KTH <pni2396@gmail.com>
2025-08-24 17:48:34 +0900

    edit: pirate

--- src/sorting/Pirate.java
+++ src/sorting/Pirate.java
@@ -1,46 +1,50 @@
 package sorting;
 
 import java.util.*;
-/*
-class Pirate {
-	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		int[][] islands = new int[n][2];
 
-		for(int i=0; i<n; i++){ //O(n)
-			islands[i][0] = sc.nextInt();
-			islands[i][1] = sc.nextInt();
-		}
+class Island implements Comparable<Island> {
+	int x, y, index;
 
-		List<int[]> sorted = sort(islands); //O(nlogn)
-		HashMap<int[], Integer> indexes = new HashMap<>();
-
-		for(int i=0; i<n; i++){ //O(n)
-			indexes.put(sorted.get(i), i);
-		}
-		
-		for(int i=0; i<n; i++){ //O(n)
-			System.out.println(n-indexes.get(islands[i])-1);
-		}
+	public Island(int x, int y, int index){
+		this.x = x;
+		this.y = y;
+		this.index = index;
 	}
-
-	public static List<int[]> sort(int[][] array){
-		List<int[]> list = new ArrayList<>(Arrays.asList(array));
-		Collections.sort(list, (a, b) -> {
-			if (a[0] == b[0] && a[1] < b[1])
-			    return -1;
-			else if (a[0] < b[0])
-			    return -1;
+	
+	@Override
+	public int compareTo(Island o){
+			if (this.x == o.x)
+			    return this.y - o.y;
 			else
-			    return 1;
-		});
-
-		return list;
+			    return this.x - o.x;
 	}
 }
-*/
+
+public class Pirate{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		ArrayList<Island> islands = new ArrayList<>();
+		
+		for(int i=0; i<n; i++){
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			islands.add(new Island(x, y, i));
+		}
+
+		Collections.sort(islands);
+		
+		int[] res = new int[n];
+		
+		for(int i=0; i<n; i++){
+			res[islands.get(i).index] = n - i -1;
+		}
+
+		for(int i=0; i<n; i++){
+			System.out.println(res[i]);
+		}
+	}
+}
 
 /*
 
@@ -53,8 +57,44 @@ class Pirate {
   --> 정렬 O(nlogn), index 검색 O(2N), but 타임아웃 발생..
 */
 
-public class Pirate{
-	public static void main(String[] args) {
-		
-	}
+/* 해시맵 기반 (좌표값을 Key로 하여 인덱스 저장)
+import java.util.*;
+
+public class Pirate {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[][] islands = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            islands[i][0] = sc.nextInt();
+            islands[i][1] = sc.nextInt();
+        }
+
+        // 정렬
+        List<int[]> sorted = new ArrayList<>(Arrays.asList(islands));
+        Collections.sort(sorted, (a, b) -> {
+            if (a[0] != b[0]) return a[0] - b[0];
+            return a[1] - b[1];
+        });
+
+        // 좌표값을 문자열로 key 사용
+        Map<String, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int[] island = sorted.get(i);
+            String key = island[0] + "," + island[1];
+            indexMap.put(key, i);
+        }
+
+        // 결과 계산
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            String key = islands[i][0] + "," + islands[i][1];
+            sb.append(n - indexMap.get(key) - 1).append('\n');
+        }
+
+        System.out.print(sb);
+    }
 }
+
+*/
